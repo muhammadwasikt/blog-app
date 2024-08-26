c31cc5c18e908bdd67b6af1d1849cbb180296228
Muhammad Wasi <wasikhatri11@gmail.com>
2024-08-26 11:34:02 -0700

    Update signup.js

--- signup/signup.js
+++ signup/signup.js
@@ -99,22 +99,15 @@ if (!/^(?=.*[a-z])(?=.*[A-Z])/.test(userName.value) && !/^[a-zA-Z0-9._-]+@[a-zA-
 googleLogin.addEventListener('click',()=>{
   signInWithPopup(auth, provider)
   .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
-    // The signed-in user info.
     const user = result.user;
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
     window.location.href = '../index.html'
   }).catch((error) => {
-    // Handle Errors here.
     const errorCode = error.code;
     const errorMessage = error.message;
-    // The email of the user's account used..
     const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
+    });
 
   
 })
